e8909eb92dcefa76b4880655a2278e3f6f0254e5
nekomehako <ngnlkkngsm@yahoo.co.jp>
2021-03-06 17:42:05 +0900

    commet out fog

--- gui.js
+++ gui.js
@@ -2,7 +2,7 @@ function addGUI(){
     // GUIパラメータ
     let guiCtrl = function(){
         this.Hex_height = 0.28;
-        this.Fog = 2.001;
+        // this.Fog = 2.001;
         this.Reflectance = 0.8;
  
     };
@@ -11,7 +11,7 @@ function addGUI(){
     guiObj = new guiCtrl();
     const building = gui.addFolder('building');
     building.add(guiObj, 'Hex_height', 0.001, 2.001);
-    building.add(guiObj, 'Fog', 0.001, 2.001);
+    // building.add(guiObj, 'Fog', 0.001, 2.001);
     building.add(guiObj, 'Reflectance', 0.001, 1.00);
 
     gui.open();
